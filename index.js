55c98d05bfe1083c51bd20c2a87f16017460183e
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-25 15:46:18 -0400

    Adding default view for layout

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
+// Modules and Globals
 require("dotenv").config();
 const express = require("express");
+const methodOverride = require("method-override");
 const app = express();
 
+// Express Settings
+app.set("view engine", "jsx");
+app.engine("jsx", require("express-react-views").createEngine());
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// Controllers & Routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.send("Hello world!");
+  res.send("Home Page");
 });
 
 app.get("*", (req, res) => {
   res.status(404).send("<h1>404 Page</h1>");
 });
 
+// Listen for Connections
 app.listen(process.env.PORT);
