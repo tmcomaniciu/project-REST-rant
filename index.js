27905e8a877be286342737f8eea46da6d6526d71
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-25 15:50:17 -0400

    Adding home page view

--- index.js
+++ index.js
@@ -1,21 +1,17 @@
 // Modules and Globals
 require("dotenv").config();
 const express = require("express");
-const methodOverride = require("method-override");
 const app = express();
 
 // Express Settings
 app.set("view engine", "jsx");
 app.engine("jsx", require("express-react-views").createEngine());
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride("_method"));
 
 // Controllers & Routes
 app.use("/places", require("./controllers/places"));
 
 app.get("/", (req, res) => {
-  res.send("Home Page");
+  res.render("Home");
 });
 
 app.get("*", (req, res) => {
