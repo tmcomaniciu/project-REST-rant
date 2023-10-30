2b3971329ba3704eac9f7ca75bb9263b68da6717
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-30 12:17:14 -0400

    show route connected

--- controllers/places.js
+++ controllers/places.js
@@ -1,6 +1,18 @@
 const router = require("express").Router();
 const Places = require("../models/data.js");
 
+//Show
+router.get("/:id", (req, res) => {
+  let id = Number(req.params.id);
+  if (isNaN(id)) {
+    res.render("error404");
+  } else if (!places[id]) {
+    res.render("error404");
+  } else {
+    res.render("places/show", { place: places[id] });
+  }
+});
+
 // GET /places/new
 router.get("/new", (req, res) => {
   res.render("places/new");
