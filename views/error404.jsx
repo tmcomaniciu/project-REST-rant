6bf6af3837da2e979a0b4a689859f9304538923e
Tudor Comaniciu <tmcomaniciu@gmail.com>
2023-10-30 20:18:30 -0400

    fixes

--- views/error404.jsx
+++ views/error404.jsx
@@ -7,7 +7,7 @@ function error404 () {
           <main>
               <h1>404: PAGE NOT FOUND</h1>
               <div>
-              <img src='public/images/ErrorPage404-05.jpg' alt='<a href="https://www.vecteezy.com/free-vector/404">404 Vectors by Vecteezy</a>' />
+              <img src='public/images/ErrorPage404-05.jpg' alt='404 Vectors by Vecteezy' />
               </div>
               <p>Oops, sorry, we can't find this page!</p>
           </main>
